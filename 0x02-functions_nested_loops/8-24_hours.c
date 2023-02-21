8853340d58084f52149cba0a07a584c34cb27dbb
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-21 07:36:16 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void print_last_digit(void)
+{
+	int n;
+	for (n = 0; n < 1440; n++)
+	{
+		int hour = n / 60;
+		int minute = n % 60;
+		_putchar(hour);
+		_putchar(':');
+		_putchar(minute);
+		_putchar('\n');
+	}
+}
