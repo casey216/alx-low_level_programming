30e87e07fd6db364fe1869d606b658a8c53265c4
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-21 12:14:00 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * add - checks if char is lowercase
+ * @a: The char to be checked
+ * @b: The char to be checked
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int add(int a, int b)
+{
+	return (a + b)
+}
