18a19e3a14d0bcbda9520f4cf690408a718bebfa
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-21 06:24:56 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints "_putchar"
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
