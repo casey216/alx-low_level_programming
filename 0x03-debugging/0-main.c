768df88e68e1ab3e07917e2c6fa14f72818cc387
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-22 00:00:24 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it.
+ * Return: 0 if no errors, return non-zero if errors
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n)
+
+	return (0);
+}
