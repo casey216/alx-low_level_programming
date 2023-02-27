e6fd10a3ee5edba6b0ff201cd8051c95fca5f1aa
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-27 06:23:05 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,5 +13,5 @@ void print_array(int *a, int n)
 	for (i = 0; i < n - 1; i++)
 		printf("%d, ", a[i]);
 
-	printf("%d", a[n - 1]);
+	printf("%d\n", a[n - 1]);
 }
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy string pointed by src to dest
+ * @dest: destination
+ * @src: source
+ * Return: pointer to a string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+	int i = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+
+	while (i <= length)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
