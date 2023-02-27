5778dd7f901728c1cb251899a6f872d615cd2dcc
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-27 07:08:44 -0800

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+/**
+ * _atoi - converts string to integer
+ * @s: string to be converted
+ * Return: converted interger, 0 if no integer is found
+ */
+
+int _atoi(char *s)
+{
+	int lenght, i, minus, number, temp;
+	bool found;
+
+	length =0;
+	i = 0;
+	minus =0;
+	number = 0;
+	found = false;
+
+	while (s[length] != '\0')
+		length++;
+
+	while (i < length && !found)
+	{
+		if (s[i] == '-')
+			minus++;
+		
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			temp = s[i];
+			number = number * 10 + temp;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+			{
+				found = true;
+				break;
+			}
+		}
+		i++;
+	}
+
+	if (!found)
+		return (0);
+
+	if (minus % 2 == 0)
+	{
+		return (number);
+	}
+	else
+	{
+		return (-number);
+	}
