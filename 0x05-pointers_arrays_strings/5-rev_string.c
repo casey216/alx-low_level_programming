4f42f94eaa647f001c92e98cbcbfb64f6450e874
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-27 05:36:23 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reverses a string variable
+ * @s: string varible
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int length, i, j;
+
+	length = 0;
+	
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	j = length - 1;
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		j--;
+	}
+
+}
