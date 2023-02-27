65b69748cf27a33c32b5585bb2e05b151bbdc5db
Kenechi Nzewi <caseynzewi@gmail.com>
2023-02-27 05:47:34 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints every other char of string
+ * @str: string varible
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int i = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	while (i < length)
+	{
+		if (i % 2 == 0)
+			_putchar(*(s + length));
+		i++;
+	}
+
+	_putchar('\n');
+}
