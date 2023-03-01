84f6d734fdb8fea29ea3d411626da16222a3e403
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-01 02:58:06 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate string src to string dest
+ * _strncat - concatenate string n bytes of src to string dest
  * @src: source string variable
  * @dest: destination string variable
+ * @n: number of bytes
  * Return: pointer to dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, len1, len2;
 
@@ -18,10 +19,13 @@ char *_strcat(char *dest, char *src)
 	while (dest[len1] != '\0')
 		len1++;
 
-	while (src[len2] != '\0')
+	while (dest[len2] != '\0')
 		len2++;
 
-	while (i <= len2)
+	if (n > len2)
+		n = len2;
+
+	while (i < n)
 	{
 		dest[i + len1] = src[i];
 		i++;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
