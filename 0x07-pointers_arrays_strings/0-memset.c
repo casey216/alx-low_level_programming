4ddbeae44c3728f15294bc845b25fcd5f02ebd9f
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-06 02:02:34 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,7 +13,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	i = 0;
 
@@ -22,4 +22,6 @@ char *_memset(char *s, char b, unsigned int n)
 		*(s + i) = b;
 		i++;
 	}
+
+	return s;
 }
