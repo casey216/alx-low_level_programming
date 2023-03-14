75b90171b910fd64950063af045a63e777513434
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-14 10:20:24 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies string
+ *
+ * @str: string to be copied
+ *
+ * Return: pointer to the copied string, otherwise NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *strcopy;
+	unsigned int i, j;
+
+	i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	strcopy = malloc(sizeof(char) * i);
+
+	if (strcopy == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		strcopy[j] = str[j];
+
+	return (strcopy);
+}
