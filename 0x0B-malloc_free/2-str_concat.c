5a0ea8797f119d67e5d7a3bb745ffb3c4b6ee33a
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-14 10:53:43 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,14 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	unsigned int i, j, k, l;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+
+	if (s1 == NULL)
+		s1 = '\0';
+
+	if (s2 == NULL)
+		s2 = '\0';
 
 	i = 0;
 	j = 0;
