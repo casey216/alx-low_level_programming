351ea372e83028832ccc29f5aa0b2269c0c10556
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-30 07:14:11 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,9 +20,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = head;
+	new->next = *head;
 
-	head = new;
+	(*head) = new;
 
-	return (head);
+	return (*head);
 }
