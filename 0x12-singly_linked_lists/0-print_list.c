d04eaf0a3faa9554f8a1bd572b95916281cb7859
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-30 04:43:55 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ *
+ * @h: list_t list
+ *
+ * Return: size of elements in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str );
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
