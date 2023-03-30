2a2b4556c47c44b8164a51620b0a56f45df2c040
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-30 02:48:31 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initializes a dog
+ *
+ * @d: pointer to dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog 
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
