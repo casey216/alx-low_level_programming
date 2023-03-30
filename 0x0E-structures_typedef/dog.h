995bbb9d891a95ee0fe7d56a8670a1ec9e968781
Kenechi Nzewi <caseynzewi@gmail.com>
2023-03-30 03:25:12 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,14 +9,12 @@
  *
  * Description: Properties of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-#define struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
